ae87c25c235b2b958ad1f6e5146c7ef666e8d2eb
yan0kom <yan0kom@gmail.com>
2023-04-14 20:16:19 +0300

    remove RequestMapping on interface (for feign)

--- rf-api/src/main/java/rf/api/RfTaskApi.java
+++ rf-api/src/main/java/rf/api/RfTaskApi.java
@@ -2,12 +2,9 @@ package rf.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import rf.api.dto.tasks.TasksOutDto;
 
-@RequestMapping("/api/tasks")
 public interface RfTaskApi {
-	@GetMapping
+	@GetMapping("/tasks")
 	ResponseEntity<TasksOutDto> list();
 }
--- rf-api/src/main/java/rf/api/RfUserApi.java
+++ rf-api/src/main/java/rf/api/RfUserApi.java
@@ -1,41 +1,33 @@
 package rf.api;
 
-import javax.validation.Valid;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import rf.api.dto.BaseOutDto;
-import rf.api.dto.users.DeleteInDto;
-import rf.api.dto.users.SignInInDto;
-import rf.api.dto.users.SignInOutDto;
-import rf.api.dto.users.SignUpInDto;
-import rf.api.dto.users.SignUpOutDto;
-import rf.api.dto.users.UsersOutDto;
+import rf.api.dto.users.*;
 
-@RequestMapping("/api/users")
+import javax.validation.Valid;
+
 public interface RfUserApi {
-	@PostMapping("sign-up")
+	@PostMapping("/users/sign-up")
 	@PreAuthorize("(isAnonymous() and #dto.role == 'USER') or hasRole('ADMIN')")
 	ResponseEntity<SignUpOutDto> signUp(@Valid @RequestBody SignUpInDto dto);
 
+	@PostMapping(value = "/users/sign-in")
 	@PreAuthorize("isAnonymous()")
-	@PostMapping("sign-in")
 	ResponseEntity<SignInOutDto> signIn(@Valid @RequestBody SignInInDto dto);
 
-	@PostMapping("sign-out")
+	@PostMapping("/users/sign-out")
 	ResponseEntity<BaseOutDto> signOut();
 
-	@GetMapping
+	@GetMapping("/users")
 	@PreAuthorize("hasRole('ADMIN')")
 	ResponseEntity<UsersOutDto> list();
 
-	@DeleteMapping("delete")
+	@DeleteMapping("/users/delete")
 	@PreAuthorize("hasRole('ADMIN')")
 	ResponseEntity<BaseOutDto> delete(@Valid @RequestBody DeleteInDto dto);
 }
