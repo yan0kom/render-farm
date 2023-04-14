29e5f3f71e912cbbf00f34a960e43716a92a22f8
yan0kom <yan0kom@gmail.com>
2023-04-14 20:12:12 +0300

    make mutable and add username

--- rf-api/src/main/java/rf/api/dto/users/SignInOutDto.java
+++ rf-api/src/main/java/rf/api/dto/users/SignInOutDto.java
@@ -3,19 +3,39 @@ package rf.api.dto.users;
 import rf.api.dto.BaseOutDto;
 
 public class SignInOutDto extends BaseOutDto {
-	private final String token;
-	private final String role;
+	private String username;
+	private String role;
+	private String token;
 
-	public SignInOutDto(String token, String role) {
+	public SignInOutDto(String username, String role, String token) {
 		super(true);
+		this.username = username;
+		this.role = role;
 		this.token = token;
+	}
+
+	public SignInOutDto() {
+		this(null, null, null);
+	}
+
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
 		this.role = role;
 	}
 
 	public String getToken() {
 		return token;
 	}
-	public String getRole() {
-		return role;
+	public void setToken(String token) {
+		this.token = token;
 	}
 }
